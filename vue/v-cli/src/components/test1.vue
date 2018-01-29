aad4cfc20f5816bd5f3e32aedfd576fc05ed45f7
Johnnaling <1337657821@qq.com>
2018-01-29 16:10:30 +0800

    vue v-cli hello 子路由

---
+++ vue/v-cli/src/components/test1.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>这是子路由1</h1>
+        <h1>姓名：{{$route.params.name}}</h1>
+        <h1>年龄：{{$route.params.age}}</h1>
+    </div>
+</template>
+<script></script>
+<style scoped>
+
+</style>
