f1ab76f3a2ac3b2e6869f0b8e42431a609856e5d
Johnnaling <1337657821@qq.com>
2018-01-30 17:09:22 +0800

    vueapp 基本架构

---
+++ vue/vueapp/src/components/common/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="footer" :style="{background:bgColor}">
+    <li><router-link to="/">电影</router-link></li>
+    <li><router-link to="/music">音乐</router-link></li>
+    <li><router-link to="/book">书籍</router-link></li>
+    <li><router-link to="/photo">图片</router-link></li>
+  </ul>
+</template>
+
+<script>
+  export default{
+      props:['bgColor']
+  }
+</script>
+
+<style scoped>
+  .footer{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 1rem;
+    display: flex;
+   /* background: #ff6700;*/
+  }
+  .footer li{
+    flex-grow: 1;
+    text-align: center;
+    line-height: 1rem;
+  }
+  .footer li a{
+    color: #ccc;
+  }
+  .footer li a.router-link-exact-active{
+    color: #fff;
+  }
+</style>
---
+++ vue/vueapp/src/components/common/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="head" :style="{background:bgColor}">
+    <span class="home">首页</span>
+    <h2 class="title">
+      <slot name="title"></slot>
+    </h2>
+
+  </div>
+</template>
+
+<script>
+  export default{
+      props:['bgColor']
+  }
+</script>
+
+<style scoped>
+.head{
+  /*background: #ff6700;*/
+  height: 1rem;
+  position: fixed;
+  width: 100%;
+  line-height: 1rem;
+}
+.title{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  color: #fff;
+}
+.home{
+  color: #fff;
+  margin-left: 0.1rem;
+}
+</style>
