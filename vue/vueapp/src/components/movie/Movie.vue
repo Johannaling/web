e1cb2d4ca410f44a42ea872f86ce18dd183a3d5d
Johnnaling <1337657821@qq.com>
2018-01-31 17:07:07 +0800

    完成部分的movie

--- vue/vueapp/src/components/movie/Movie.vue
+++ vue/vueapp/src/components/movie/Movie.vue
@@ -1,15 +1,51 @@
 <template>
   <div>
+    <ul class="list">
+      <li class="movie" v-for="movie in movieList" :key="movie.id">
+        <div class="movie-img">
+          <img :src="movie.img" alt="">
+        </div>
+        <div class="movie-info">
+          <p v-text="movie.nm"></p>
+          <p v-text=""></p>
+        </div>
 
+      </li>
+    </ul>
   </div>
 
 </template>
 
 <script>
-
+  import axios from 'axios'
 
   export default{
+    data(){
+      return{
+        movieList:[]
+      }
+    },
+    methods:{
+      getMovieData(){
+        axios.get(
+          API_PROXY+`http://m.maoyan.com/movie/list.json?type=hot&offset=0&limit=10`
+        ).then(
+          res =>{
+            console.log(res);
+            let list = res.data.data.movies;
+            this.movieList=list
+          }
 
+        ).catch(
+          res => {
+            alert('数据丢了┌╏ º □ º ╏┐')
+          }
+        )
+      }
+    },
+    created() {
+      this.getMovieData();
+    }
   }
 </script>
 
