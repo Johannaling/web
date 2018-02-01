ad7018074a5316d59be946a9d2896732b04fd330
Johnnaling <1337657821@qq.com>
2018-02-01 21:09:26 +0800

    完成部分MovieDetail

---
+++ vue/vueapp/src/components/movie/Moviedetail.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="detail">
+
+      <div class="detail-info" >
+        <div class="detail-background" :style="{background:'url('+detail.img+')'}"></div>
+        <div class="detail-desricption">
+          <div class="detail-img">
+            <img :src="detail.img" alt="">
+          </div>
+          <div class="detail-content">
+            <p class="detail-name">{{ detail.nm }}</p>
+            <p>{{ detail.dir }}</p>
+            <p>{{ detail.cat }}</p>
+            <p>{{ detail.src }}</p>
+            <p>{{ detail.rt }}</p>
+          </div>
+        </div>
+      </div>
+    <div class="detail-context">
+      <div class="detail-star">
+        <h3>主演</h3>
+        <p>{{ detail.star }}</p>
+      </div>
+      <div>
+        <h3>简介</h3>
+        <div v-html="detail.dra"></div>
+      </div>
+      <div class="loading" v-show="isLoading">
+        <img src="../../assets/img/load.gif" alt="">
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+  export default{
+    data() {
+      return {
+        detail: {},
+        isLoading: true
+      };
+    },
+    created() {
+      axios
+        .get(
+          `${API_PROXY}http://m.maoyan.com/movie/${
+            this.$route.params.movieId
+            }.json`
+        )
+        .then(res => {
+            console.log(res);
+          this.detail = res.data.data.MovieDetailModel;
+          this.isLoading = false;
+        })
+        .catch(res => {
+          alert("error");
+        });
+    }
+
+  }
+
+</script>
+
+<style scoped>
+  .detail {
+
+  }
+  .detail-background{
+    filter:blur(0.6rem);
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    background-repeat: no-repeat;
+  }
+  .detail-info {
+    /*display: flex;*/
+    border-bottom: 1px solid #ccc;
+
+
+  }
+  .detail-desricption{
+    position: relative;
+    display: flex;
+    padding-top: 0.1rem;
+    background: rgba(64,69,77,0.2);
+    padding-bottom: 0.1rem;
+  }
+  .detail-img {
+    flex-grow: 1;
+    width: 0;
+    z-index: 1;
+    border: 1px solid #fff;
+    margin-right: 0.2rem;
+    margin-left: 0.2rem;
+  }
+  .detail-content {
+    flex-grow: 2;
+    width: 0;
+    z-index: 1;
+    color: #fff;
+  }
+  .detail-name {
+    font-weight: bolder;
+  }
+
+  .detail h3 {
+    text-align: center;
+    font-weight: bolder;
+  }
+
+  .detail-star {
+    border-bottom: 1px solid #ccc;
+
+  }
+  .loading {
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .detail-context{
+    background: #fff;
+    position: relative;
+
+  }
+</style>
