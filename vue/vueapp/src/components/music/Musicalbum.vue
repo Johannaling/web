129315c218b1c78f381c4035bf3b0ba768446647
Johnnaling <1337657821@qq.com>
2018-02-03 13:45:43 +0800

    musicalbum

---
+++ vue/vueapp/src/components/music/Musicalbum.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <aplayer autoplay :music="musiclist" v-if="loadSuccess" :showlrc="true" :mutex="true"></aplayer>
+  </div>
+</template>
+
+<script>
+  import Aplayer from "vue-aplayer";
+  import axios from 'axios';
+
+  export default{
+    components: {
+      Aplayer
+    },
+    data(){
+        return{
+          musiclist: [],
+          loadSuccess: false
+        }
+    },
+    created(){
+        axios.get('/static/data/musicdata.json').then(res => {
+          res.data.musicData.forEach(element => {
+            this.musiclist.push({
+              title: element.title,
+              author: element.author ,
+              url: element.src,
+              pic: element.musicImgSrc ,
+              lrc: '/static/' + element.lrc
+            });
+          });
+          this.loadSuccess=true
+        })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
