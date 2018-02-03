e2d672d72869f1dce4ffc257b403fc26fcfbf119
Johnnaling <1337657821@qq.com>
2018-02-03 10:37:51 +0800

    music

--- vue/vueapp/src/components/music/Music.vue
+++ vue/vueapp/src/components/music/Music.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-
-  </div>
+  <ul>
+    <li v-for="item in billboard" :key="item.id" class="billboard">
+      <img :src="item.bg" alt="">
+    </li>
+  </ul>
 </template>
 
 <script>
+  import axios from 'axios'
   export default{
-
+    data(){
+        return{
+          billboard:[]
+        }
+    },
+    created(){
+        axios.get('/static/data/musiclist.json').then(res => {
+            console.log(res);
+            this.billboard=res.data.albums;
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .billboard{
+    width: 50%;
+    float: left;
+  }
+  .billboard img{
+    width: 100%;
+  }
 </style>
