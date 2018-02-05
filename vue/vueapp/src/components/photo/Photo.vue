d0437d1aee5d2726ea96b5cd51efaab61743bf2d
Johnnaling <1337657821@qq.com>
2018-02-05 11:07:21 +0800

    photo

--- vue/vueapp/src/components/photo/Photo.vue
+++ vue/vueapp/src/components/photo/Photo.vue
@@ -1,16 +1,44 @@
 <template>
-  <div>
+  <ul class="clearfix">
+    <li v-for="(item, index) in photoList" :key="index" class="photo" >
+      <router-link :to="'/photodetail/'+ (index+1)" @click.native="$emit('length','photoList.length')">
+        <img :src="item.src" alt="">
+      </router-link>
 
-  </div>
+    </li>
+  </ul>
 </template>
 
 <script>
-
+  import axios from 'axios'
   export default{
+    data(){
+      return{
+        photoList:[]
+      }
+    },
+    created(){
+      axios.get('/static/data/photodata.json').then(res =>{
+
+        this.photoList=res.data.photoData;
+        /*console.log(this.photoList.length);*/
+      }).catch(() =>{
+        alert('数据出错了')
+
+      })
+    },
 
   }
 </script>
 
 <style scoped>
-
+  .photo{
+    width: 50%;
+    float: left;
+  }
+  .clearfix::after{
+    content: '';
+    display: block;
+    clear: both;
+  }
 </style>
