787ed9c19c4f9a80330d57436ed7713e11cd079e
Johnnaling <1337657821@qq.com>
2018-03-18 21:53:25 +0800

    vuex

---
+++ vue/vueapp/src/vuex/store.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Johnnaling on 2018/2/4.
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state={
+
+};
+const mutations={
+
+};
+const getters={
+
+};
+const actions={
+
+};
+export default new Vuex.Store({
+  state,
+  mutations,
+  getters,
+  actions
+})
